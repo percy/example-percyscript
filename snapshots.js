71b1efdacc565a14d7d3e5d3b35472efb1d791b1
ranu <ranushka543@gmail.com>
2020-06-14 11:35:54 +0400

    aaaaa

--- snapshots.js
+++ snapshots.js
@@ -1,13 +1,16 @@
-const PercyScript = require('@percy/script');
+const PercyScript = require("@percy/script");
+
+const takeShots = async (name) => {
+  await percySnapshot("", { widths: [768, 992, 1200] });
+};
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:8000');
-  await percySnapshot('TodoMVC home page');
+  await page.goto("http://localhost:8000");
+  await takeShots("TodoMVC home page");
 
   // Enter a new to-do.
-  await page.type('.new-todo', 'A really important todo');
-  await page.keyboard.press('Enter');
-  await percySnapshot('TodoMVC with a new todo', { widths: [768, 992, 1200] });
+  await page.type(".new-todo", "A really important todo");
+  await page.keyboard.press("Enter");
+  await takeShots("TodoMVC with a new todo");
 });
-
