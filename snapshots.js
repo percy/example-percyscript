5ba8cf5fd197c96c232924de4055b5c02f2e5ae2
Robert DeLuca <robertdeluca19@gmail.com>
2020-07-13 14:11:33 -0500

    fix: Start server from node script
    This skips any complexity windows introduces by not supporting bash
    commands (the npm scripts used bash which won't work in Windows/Powershell)

--- snapshots.js
+++ snapshots.js
@@ -1,13 +1,22 @@
 const PercyScript = require('@percy/script');
+const httpServer = require('http-server');
+
+const PORT = process.env.PORT_NUMBER || 8000;
+const TEST_URL = `http://localhost:${PORT}`;
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:8000');
+  let server = httpServer.createServer();
+  server.listen(PORT);
+
+  console.log(`Server started at ${TEST_URL}`);
+
+  await page.goto(TEST_URL);
   await percySnapshot('TodoMVC home page');
 
   // Enter a new to-do.
   await page.type('.new-todo', 'A really important todo');
   await page.keyboard.press('Enter');
   await percySnapshot('TodoMVC with a new todo', { widths: [768, 992, 1200] });
+  server.close();
 });
-
